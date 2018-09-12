8319ed211df989b9beee4899365ee3ab3445df18
restrellado <ryan.estrellado@gmail.com>
2018-09-11 21:17:26 -0700

    Add function to rename column names

--- clean_data.R
+++ clean_data.R
@@ -1,10 +1,18 @@
 library(tidyverse)
 source("load_data.R") 
 
+# Read in files 
 all_data <- list.files("data", full.names = TRUE) %>% 
   map(~read_csv(., col_names = FALSE))
 
-all_data[[1]] %>% 
-  filter(!(X1 %in% c("Created", "Updated", "Revised")), 
-         # These are NAs in all columns
-         !is.na(X1))
+test_func <- function(d) {
+  # Removes empty rows at the top and renames column names
+  d <- d %>% 
+    filter(!is.na(X3)) 
+  # Rename headings using first row
+  names(d) <- as.character(d[1, ])
+  # Remove first row
+  d %>% filter(row_number() != 1)
+}
+
+all_data %>% map(test_func)
